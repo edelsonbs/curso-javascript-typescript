c44df736229fcf34ae52d279a0950026fe1da1e7
Edelson Barboza <edelsonbsilva@gmail.com>
2024-09-19 14:36:05 -0300

    alert, confirm e prompt

---
+++ secao-2/aula-11/js/script.js
@@ -0,0 +1,24 @@
+// alert('Mesnsagem de alerta');
+
+// const confirma = confirm('Confirma que deseja apagar?');
+
+// if(confirma) {
+//     document.write('Apagou!');
+// }
+
+// console.log(confirma);
+
+// Exercício
+let num1 = prompt('Digite um número: ');
+let num2 = prompt('Digite outro número: ');
+
+num1 = parseFloat(num1);
+num2 = Number(num2);
+
+console.log(typeof num1);
+console.log(typeof num2);
+
+// const resultado = num1 + num2;
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
+console.log();
