b46376a0fe8454a891f4c0abf072c598fddada3d
Edelson Barboza <edelsonbsilva@gmail.com>
2024-09-19 14:00:00 -0300

    Operadores aritméticos, de atribuição e incremento

---
+++ secao-2/aula-10.js
@@ -0,0 +1,37 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação
+ * - * /
+ * ** Potenciação
+ * % Resto da divisão
+ * ++ Incremento
+ * -- Decremento
+ * NaN - Not a Number
+ * parseInt() - Converte para inteiro
+ * parseFloat() - Converte para float
+ */
+const num1 = 10;
+const num2 = 3;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 ** num2);
+console.log(num1 % num2);
+
+// let contador = 1;
+// contador++; // 2
+// contador++; // 3
+// ++contador; // 4
+// console.log(contador);
+
+// contador--; // 3
+// console.log(contador);
+
+let contador = 2;
+// contador += 2;
+// contador += 2;
+// contador *= 2;
+contador **= 10;
+console.log(contador);
