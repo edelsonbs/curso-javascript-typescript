202bd01bf288e33ff5973e85ad8c66d3e4fe3df1
Edelson Barboza <edelsonbsilva@gmail.com>
2024-09-19 13:45:39 -0300

    Let vs Var - Primeira diferença

---
+++ secao-2/aula-9.js
@@ -0,0 +1,21 @@
+var nome = 'Luiz';
+var nome = 'Otávio';
+
+console.log(nome);
+
+// Tipos Primitivos -> string, number, boolean, null, undefined
+
+const nome = "Luiz"; // string
+const nome1 = "Luiz"; // string
+const nome2 = `Luiz`; // string
+const num1 = 10; // number
+const num2 = 10.52; // number
+let nomeAluno; // undefined -> não aponta para local nenhum da memória
+const sobreNomeAluno = null; // Nulo -> não aponta para local nenhum da memória
+const aprovado = true; // Boolean = true ou false (lógico)
+
+console.log(typeof nome, nome);
+console.log(typeof num1, num1);
+console.log(typeof nomeAluno, nomeAluno);
+console.log(typeof sobreNomeAluno, sobreNomeAluno);
+console.log(typeof aprovado, aprovado);
