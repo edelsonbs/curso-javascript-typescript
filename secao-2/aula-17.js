1abf72657d60118599cd2df4dca6752b510752d7
Edelson Barboza <edelsonbsilva@gmail.com>
2024-09-19 14:57:06 -0300

    Funções em JavaScript

---
+++ secao-2/aula-17.js
@@ -0,0 +1,33 @@
+// function saudacao(nome) {
+//   return `Bom dia, ${nome}!`;
+// }
+
+// const variavel = saudacao('Luiz');
+// // saudacao('Maria');
+// // saudacao('Felipe');
+// console.log(variavel);
+
+// function soma(x = 1, y = 1) {
+//   const resultado = x + y;
+//   return resultado;
+// }
+
+// // console.log(soma(2, 2));
+// // console.log(soma(3, 1));
+// // console.log(soma(5, 10));
+// const resultado = soma(4);
+// console.log(soma(resultado));
+
+// const raiz = function (n) {
+//   return n ** 0.5;
+// };
+
+// const raiz = (n) => {
+//   return n ** 0.5;
+// };
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9));
+console.log(raiz(16));
+console.log(raiz(25));
