788cd6c2212e65fa03c823d98e958c5a7e281571
Edelson Barboza <edelsonbsilva@gmail.com>
2024-09-19 17:15:15 -0300

    Funções imediatas (IIFE)

---
+++ secao-4/aula-55.js
@@ -0,0 +1,23 @@
+// IIFE -> Immediately Invoked Function Expression
+// Sintaxe:
+// (function() {
+  
+// })();
+
+(function(idade, peso, altura) {
+    const sobrenome = 'Miranda';
+    function criaNome(nome) {
+      return nome + ' ' + sobrenome;
+    }
+  
+    function falaNome() {
+      console.log(criaNome('Luiz')); 
+    }
+  
+    falaNome();
+    console.log(idade, peso, altura);
+    
+  })(30, 80, 1.80);
+  
+  const nome = 'Qualquer coisa';
+  
