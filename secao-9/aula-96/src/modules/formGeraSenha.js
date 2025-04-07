a84531fc05bf598d8dc30ddd50c02902f63996d4
Edelson Barboza <edelsonbsilva@gmail.com>
2025-04-06 21:52:25 -0300

    Criando um gerador de senhas

---
+++ secao-9/aula-96/src/modules/formGeraSenha.js
@@ -0,0 +1,28 @@
+import geraSenha from './geradores';
+
+const senhaGerada = document.querySelector('.senha-gerada');
+const qtdCaracteres = document.querySelector('.qtd-caracteres');
+const chkMaisculas = document.querySelector('.chk-maiusculas');
+const chkMinusculas = document.querySelector('.chk-minusculas');
+const chkNumeros = document.querySelector('.chk-numeros');
+const chkSimbolos = document.querySelector('.chk-simbolos');
+const gerarSenha = document.querySelector('.gerar-senha');
+
+export default () => {
+    gerarSenha.addEventListener('click', ()=> {
+       senhaGerada.innerHTML = gera(); 
+    });
+    // console.log('Olá mundão!')
+}
+
+function gera() {
+    const senha = geraSenha(
+        qtdCaracteres.value,
+        chkMaisculas.checked,
+        chkMinusculas.checked,
+        chkNumeros.checked,
+        chkSimbolos.checked
+    );
+
+     return senha || 'Nada selecionado';
+}
