1eb7e3e2074c94edb73bc55637266ce931959f04
Edelson Barboza <edelsonbsilva@gmail.com>
2025-04-17 01:30:59 -0300

    Módulos (1)

---
+++ secao-10/aula-1/mod1.js
@@ -0,0 +1,35 @@
+// const nome = 'Luiz';
+// const sobrenome = 'Miranda';
+
+// const falaNome = () => nome + ' ' + sobrenome;
+
+// // module.exports.nome = nome;
+// // module.exports.sobrenome = sobrenome;
+// // module.exports.falaNome = falaNome;
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
+// this.qualquerCoisa = 'O que eu quiser exportar';
+
+// // console.log(module.exports);
+// // console.log(exports);
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome;
+    }
+}
+
+const nome = 'Luiz';
+const sobrenome = 'Miranda';
+
+exports.Pessoa = Pessoa;
+
+// module.exports = {
+//     nome, sobrenome, Pessoa
+// }
+
+exports.nome = nome;
+module.exports.sobrenome = sobrenome;
+exports.outraCoisa = 'Outra coisa';
